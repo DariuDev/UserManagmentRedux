5548af604a52ef224d4dc30e16d50b8d3464e007
dariush <dariushtaghavi9797@gmail.com>
2022-06-12 14:22:15 +0430

    add search

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
  import thunk from 'redux-thunk';
- import paginationReducer from '../store/reducers/Reducers';
+ import userReducer from './reducers/Reducers';
 
-
- export const rootReducer = combineReducers({paginationReducer});
+ export const rootReducer = combineReducers({userReducer});
 
  export const Store = createStore(rootReducer, applyMiddleware(thunk));
